6d0617fbedea69fb9f841803da295b4ee3745c4d
medev21 <benmarteen07@gmail.com>
2019-01-19 15:55:39 -0500

    set up basic router in users.js

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,8 +1,10 @@
 const express = require('express');
-const router = express.router();
+const router = express.Router();
 
 router.get('/', (req,res,next) => {
     res.status(200).json({
         message: 'hello world'
     });
 });
+
+module.exports = router;
